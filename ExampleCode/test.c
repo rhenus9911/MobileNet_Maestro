6ebd37feeba58563aaf95b5fbb7f15492b5d9d5c
Sangji0559 <gkgktkdwl@gmail.com>
2024-04-03 19:49:22 +0100

    change code

--- ExampleCode/test.c
+++ ExampleCode/test.c
@@ -1,22 +1,33 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#define _GNU_SOURCE
 
 int main(int argc, char **argv)
 {
-	
-	FILE *cpuinfo=popen("cat /proc/cpuinfo | grep processor | wc -l", "rb");
-	char buff[100];
-	memset(buff,0x00,sizeof(buff));
-	printf("%s",buff);
-	if(buff[0]=='4'){
-		printf("cpu is alright");
-		
-	}
-	else
-		printf("cpu is borken");
-	pclose(cpuinfo);
-	return 0;
-}
+    FILE *cpuinfo;
+    char buffer[1024]; // 버퍼 크기는 적당히 조정해야 합니다.
 
+    // 쉘을 통해 명령어 실행
+    cpuinfo = popen("grep processor /proc/cpuinfo | wc -l", "r");
+
+    // popen이 제대로 실행되었는지 확인
+    if(cpuinfo == NULL){
+        printf("Failed to run command\n");
+        return 1;
+    }
+
+    // 명령어의 출력을 읽어옴
+    if(fgets(buffer, sizeof(buffer), cpuinfo) != NULL){
+        int num_processors = atoi(buffer);
+        printf("Number of processors: %d\n", num_processors);
+    }
+    else {
+        printf("Failed to read output\n");
+        return 1;
+    }
+
+    // popen 종료
+    pclose(cpuinfo);
+
+    return 0;
+}
