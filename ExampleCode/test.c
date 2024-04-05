53e83f345c539413e86d34f522d76c5c16061281
Sangji0559 <gkgktkdwl@gmail.com>
2024-04-05 09:41:58 +0100

    change code

--- ExampleCode/test.c
+++ ExampleCode/test.c
@@ -5,29 +5,44 @@
 int main(int argc, char **argv)
 {
     FILE *cpuinfo;
-    char buffer[1024]; // 버퍼 크기는 적당히 조정해야 합니다.
+    FILE *modelname;
+    FILE *lscpu;
 
-    // 쉘을 통해 명령어 실행
+    char buffer[1024]; 
+
     cpuinfo = popen("grep processor /proc/cpuinfo | wc -l", "r");
+    modelname = popen("grep Model /proc/cpuinfo","r");
+    lscpu = popen("lscpu | grep 'CPU'", "r");
 
-    // popen이 제대로 실행되었는지 확인
+
     if(cpuinfo == NULL){
         printf("Failed to run command\n");
         return 1;
     }
 
-    // 명령어의 출력을 읽어옴
+    while(fgets(buffer,sizeof(buffer),lscpu)){
+        printf("%s",buffer);
+    }
+
+    if(fgets(buffer, sizeof(buffer), modelname) != NULL){
+        printf("%s",buffer);
+    }
+
     if(fgets(buffer, sizeof(buffer), cpuinfo) != NULL){
         int num_processors = atoi(buffer);
         printf("Number of processors: %d\n", num_processors);
+        if(num_processors==4){
+            printf("Proccesor is alright\n");
+        }
     }
     else {
         printf("Failed to read output\n");
         return 1;
     }
 
-    // popen 종료
     pclose(cpuinfo);
+    pclose(modelname);
+    pclose(lscpu);
 
     return 0;
 }
