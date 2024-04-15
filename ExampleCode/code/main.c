e7a3e0593c579596b2547da59d176de25ba3093d
1829122 <100738276+1829122@users.noreply.github.com>
2024-04-15 18:27:11 +0900

    Create main.c

---
+++ ExampleCode/code/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test22
+//
+//  Created by 김남호 on 2024/04/15.
+//
+
+#include <stdio.h>
+#include "test.h"
+
+int main(int argc, const char * argv[]) {
+    Gpio_Test();
+    Cpu_Test();
+    return 0;
+}
