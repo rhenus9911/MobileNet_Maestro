dc5ea28b54d1fcf3bdd74c0f15c8f321e3da498c
1829122 <100738276+1829122@users.noreply.github.com>
2024-05-17 16:46:27 +0900

    Update PWMtest.c

--- ExampleCode/PWMtest.c
+++ ExampleCode/PWMtest.c
@@ -1,28 +1,47 @@
 #include <wiringPi.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <softPwm.h>
 
-#define LED 1
+const int BCM12_PWM0 = 12;   // pwm 핀 선언
+const int BCM18_PWM0 = 13;
+const int BCM13_PWM1 = 18;
 
-int bright = 0;
+int main(){
+	int intensity;
+ 	wiringPiSetupGpio();      // BCM 핀번호 사용
+	pwmSetMode(PWM_MODE_MS);  // 마크-스페이스 모드 사용
+	pinMode(BCM12_PWM0, OUTPUT);   // 일반 GPIO 핀으로 설정
+	pinMode(BCM18_PWM0, OUTPUT);   // 일반 GPIO 핀으로 설정
+	pinMode(BCM13_PWM1, OUTPUT);   // 일반 GPIO 핀으로 설정
+	
+	softPwmCreate(BCM12_PWM0, 1, 100);   // SOFTWARE PWM에서 사용할 범위 지정, 여기서는 0-100
+	softPwmCreate(BCM18_PWM0, 1, 100);
+	softPwmCreate(BCM13_PWM1, 1, 100);
 
-int main(void){
-    wiringPiSetup();
-    
-    pinMode(LED, PWM_OUTPUT);
-    digitalWrite(LED, LOW);
-    
-    
-    while(1){
-        for(int i = 0; i < 100; i++){
-            pwmWrite(LED, i);
-            delay(50);
-                
-        }
-        for(int i = 0; i < 100; i++){
-            pwmWrite(LED, i);
-            delay(50);
-        
-        }
-    }
+	while(1){
+	
+		for(intensity=0; intensity<100; ++intensity){
+			softPwmWrite(BCM12_PWM0, intensity / 2);
+			softPwmWrite(BCM18_PWM0, 100-intensity);	
+			softPwmWrite(BCM13_PWM1, intensity);	
 
+			delay(1);
+
+		}	
+
+		
+		for(intensity=100; intensity>=0; --intensity){
+			softPwmWrite(BCM12_PWM0, 50);	
+			softPwmWrite(BCM18_PWM0, 100-intensity);	
+			softPwmWrite(BCM13_PWM1, intensity);	
+			delay(1);
+
+		}	
+		delay(1);
+	
+	
+	} 
+
+	return 0;
 }
