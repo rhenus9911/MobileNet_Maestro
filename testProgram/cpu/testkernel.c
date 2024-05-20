c6c0b22ae4d6ade1910525f5295db7da26b692f2
Sangji0559 <gkgktkdwl@gmail.com>
2024-05-20 06:09:35 +0100

    test kernel module

---
+++ testProgram/cpu/testkernel.c
@@ -0,0 +1,16 @@
+#include<linux/module.h>
+
+MODULE_LICENSE("GPL");
+
+
+static int test_init(void){
+    printk(KERN_INFO "OK HELLO\n");
+    return 0;
+}
+
+static int test_exit(void){
+    printk(KERN_INFO "OK BYE\n");
+}
+
+module_init(test_init);
+module_init(test_exit);
