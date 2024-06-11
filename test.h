4dc11e36886e943d350dcfe27a71dcac4a32412e
1829122 <100738276+1829122@users.noreply.github.com>
2024-06-11 13:51:33 +0900

    Create test.h

---
+++ test.h
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <string.h>
+#include <wiringPiSPI.h>
+#include <wiringPi.h>
+#include <sys/types.h>
+#include <ifaddrs.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/hci.h>
+#include <bluetooth/hci_lib.h>
+#include <bluetooth/rfcomm.h>
+#include <sys/socket.h>
+
+
+
+
+
+
+int GpioTest();
+void setup();
+int logPWMInput();
+int PWMTest();
+int spi_loopback_test(int channel);
+int SPITest_0();
+int SPITest_1();
+int wifiTest();
+//이더넷부터
+char* get_ip_address();
+void check_network_interface();
+void ping_test(const char* ip_address);
+void iperf_test(const char* server_ip);
+int EthernetTest();
+int bluetoothTest();
+
+
+
