9c45c23d229a8d79c9c6dbb32cfdc1bb60bb2609
rhenus9911 <jeonghoon9911@naver.com>
2024-06-13 09:39:03 +0900

    modify memTest

--- testProgram/memory/memTestProgram.c
+++ testProgram/memory/memTestProgram.c
@@ -137,11 +137,12 @@ void memoryBandWidthCheck()
 {
     double times[3][NTIMES];
     double avg_t[3], min_t[3], max_t[3], best_rate[3];
-    double *array = (double *)malloc(STREAM_ARRAY_SIZE * sizeof(double));
-    
-    for(int k=0; k<STREAM_ARRAY_SIZE; k++)
+    array = (double *)malloc(STREAM_ARRAY_SIZE * sizeof(double));
+    printf("[+]Memory BandWitch Check\n");
+
+    for(int k=0;k<STREAM_ARRAY_SIZE;k++)
     {
-        
+        a[k] = 1.0;
         b[k] = 0.0;
         array[k] = 0.0;
     }
@@ -149,25 +150,25 @@ void memoryBandWidthCheck()
     for(int k=0;k<NTIMES;k++)
     {
         double start = timeCheck();
-        readBandWidth(array);
+        copyBandWidth();
         double end = timeCheck();
-        times[0][k] = end - start;
+        times[0][k] = end-start;
     }
 
     for(int k=0;k<NTIMES;k++)
     {
         double start = timeCheck();
-        writeBandWidth(array);
+        readBandWidth();
         double end = timeCheck();
-        times[1][k] = end - start;
+        times[1][k] = end-start;
     }
 
     for(int k=0;k<NTIMES;k++)
     {
         double start = timeCheck();
-        copyBandWidth(a, b);
+        writeBandWidth();
         double end = timeCheck();
-        times[2][k] = end - start;
+        times[2][k] = end-start;
     }
 
     for(int i=0;i<3;i++)
@@ -186,10 +187,10 @@ void memoryBandWidthCheck()
 
         best_rate[i] = (2.0 * STREAM_ARRAY_SIZE * sizeof(double)) / (min_t[i] * 1.0e6);
     }
-    printf("    Function    Best Rate MB/s  Avg time    Min time    Max time\n");
-    printf("    Read:   %11.2lf         %8.6lf    %8.6lf    %8.6lf\n", best_rate[0], avg_t[0], min_t[0], max_t[0]);
-    printf("    Write:  %11.2lf         %8.6lf    %8.6lf    %8.6lf\n", best_rate[1], avg_t[1], min_t[1], max_t[1]);
-    printf("    Copy:   %11.2lf         %8.6lf    %8.6lf    %8.6lf\n", best_rate[2], avg_t[2], min_t[2], max_t[2]);
+    printf("Function    Best Rate MB/s  Avg time    Min time    Max time\n");
+    printf("Copy:   %11.2lf         %8.6lf    %8.6lf    %8.6lf\n", best_rate[0], avg_t[0], min_t[0], max_t[0]);
+    printf("Read:   %11.2lf         %8.6lf    %8.6lf    %8.6lf\n", best_rate[1], avg_t[1], min_t[1], max_t[1]);
+    printf("Write:  %11.2lf         %8.6lf    %8.6lf    %8.6lf\n", best_rate[2], avg_t[2], min_t[2], max_t[2]);
 
     if(avg_t[0] >= 4000 && avg_t[1] >= 1600 && avg_t[2] >= 1500) funcCheck[2] = 2;
     else if(avg_t[0] < 4000 && avg_t[1] < 1600 && avg_t[2] < 1500) funcCheck[2] = 0;
